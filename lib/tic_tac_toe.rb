eb433ee0a4fb9ff77465a12fc392c8e7c35a8239
Aaron Brady <aleebrady@gmail.com>
2020-08-29 23:09:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ class TicTacToe
     
     def won? 
       WIN_COMBINATIONS.detect do |w|
-        if @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]] && position_taken?(win[0])
+        if @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]] && position_taken?(w[0])
           w
     end
   end
