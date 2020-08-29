f925823046a408c33ce3e0e0cf0ff8e39f1250c1
Aaron Brady <aleebrady@gmail.com>
2020-08-29 22:57:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TicTacToe
   
   def initialize(board = nil)
@@ -16,18 +18,18 @@ class TicTacToe
     ]
     
     def display_board
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-      puts "-----------"
-      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts "-----------"
-      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+       puts "-----------"
+       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+       puts "-----------"
+       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
     
-    def input_to_index(input_turn)
-      input_turn.to_i - 1 
+    def input_to_index(input)
+      input.to_i - 1
     end
     
-    def move(index, token)
+    def move(index, token = X)
       @board[index] = token
     end
     
@@ -39,66 +41,23 @@ class TicTacToe
       index.between?(0,8) && !position_taken?(index)
     end
     
-   def turn_count()
-     @board.count {|i| i == "X" || i == "O"}
-   end
-   
-   def current_player
-     turn_count % 2 == 0 ? "X" : "O"
-   end
-   
-   def turn 
-     puts "Enter a number 1-9"
-     input = gets.chomp
-     index = input_to_index(input)
-     if valid_move?(index)
-       move(index, current_player)
-       display_board
-     else
-       turn
-     end
-   end
-   
-   def won?
-     WIN_COMBINATIONS.detect do |win|
-       if @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]] && position_taken?(win[0])
-         win
-       end
-     end
-   end
-   
-   def full?
-     @board.all? {|i| i == "X" || i == "O"}
-   end
-   
-   def draw?
-     full? && !won?
-   end
-   
-   def over?
-     won? || full?
-   end
-   
-   def winner
-    if  winner = won?
-     @board[winner.first]
-   end
- end
+    def turn_count
+      @board.count {|i| i=="X" || i == "O"}
+    end
+    
+    def current_player
+      turn_count % 2 == 0 ? "X" : "O"
+    end
+    
+    def turn 
+      
+    end
 
-   def play
-    until over?
-    turn
-    break if draw?
- end
- 
- if won?
-     puts "Congratulations #{winner}!"
- else
-   draw?
-   puts "Cat's Game!"
- end
- end
-   
-   
-  
 end
+
+
+
+ 
+
+
+
